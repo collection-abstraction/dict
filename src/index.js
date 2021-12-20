70468f68af90c0e551e05f92091b1e9a72dd266a
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2021-12-20 11:33:32 +0100

    :arrow_up: deps: Upgrade xo to v0.47.0.

--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@ import _dict from './_dict.js';
 const Dict = _Dict(Map); // Use native Map implementation
 const dict = _dict(Dict);
 
-export {dict, Dict, _dict, _Dict};
+export {dict, Dict};
+export {default as _dict} from './_dict.js';
+export {default as _Dict} from './_Dict.js';
